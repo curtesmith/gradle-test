2ce9e4ee583c8d69ccde242da4def62866f5d717
curtesmith <curt.e.smith@gmail.com>
2014-02-02 16:27:28 -0500

    testing junit

---
+++ android-application/ChineseCheckers/src/main/java/ca/brocku/chinesecheckers/MainActivityTest.java
@@ -0,0 +1,28 @@
+package ca.brocku.chinesecheckers;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Curtis on 02/02/14.
+ */
+public class MainActivityTest extends TestCase {
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testOnCreate() throws Exception {
+        fail("onCreate is not implemented");
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+        fail("onCreateOptionsMenu is not implemented");
+    }
+
+    public void testOnOptionsItemSelected() throws Exception {
+        assertTrue("this should not fail", true);
+    }
+}
